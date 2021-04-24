0a037c5dbf6777466dbfe20596f76017291c0729
Slawomir Kaczynski <slawomir.kaczynski@billennium.com>
2021-04-24 17:11:44 +0200

    Add app

---
+++ src/components/Loader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="text-center">
+    <v-progress-circular
+      :size="70"
+      :width="7"
+      color="purple"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
+
+<style scoped>
+.text-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 100%;
+}
+</style>
